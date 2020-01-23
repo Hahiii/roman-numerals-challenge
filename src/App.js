2e4c6d98603c5f54313da3d0219d646d1d2be78f
Nazim Shala <shalanazim@gmail.com>
2020-01-24 00:01:57 +0100

    add UI

--- src/App.js
+++ src/App.js
@@ -36,8 +36,42 @@ function App() {
         <button onClick={() => calculate()}>
           Click
         </button>
-        
-        {res && <h2> this is the res: {res} </h2>}
+          
+        {res && <h2> this is the res: {res} </h2>} */}
+
+        <div className="box">
+          <div className="screen">
+            <h1 ref={inOutPut}></h1>
+          </div>
+          <div className="keyboard">
+            <div className="controlls-container">
+              <button className="controll one active" id="reset" data-value="reset" onClick={(e) => handleClick(e.target)}>reset</button>
+              <button className="controll two active" id="reset" data-value="C" onClick={(e) => handleClick(e.target)}>C</button>
+              <button className="controll tree active" id="reset" data-value="=" onClick={(e) => handleClick(e.target)}>=</button>
+            </div>
+            <div className="values-container">
+              <div className="values">
+                <button className="val-item active" id="value" data-value="C" onClick={(e) => handleClick(e.target)}>C</button>
+                <button className="val-item active" id="value" data-value="D" onClick={(e) => handleClick(e.target)}>D</button>
+                <button className="val-item active" id="value" data-value="M" onClick={(e) => handleClick(e.target)}>M</button>
+                <button className="val-item active" id="value" data-value="V" onClick={(e) => handleClick(e.target)}>V</button>
+                <button className="val-item active" id="value" data-value="X" onClick={(e) => handleClick(e.target)}>X</button>
+                <button className="val-item active" id="value" data-value="L" onClick={(e) => handleClick(e.target)}>L</button>
+                <button className="val-item active" id="value" data-value="I" onClick={(e) => handleClick(e.target)}>I</button>
+                <button className="val-item">.</button>
+                <button className="val-item">S</button>
+              </div>
+              <div className="calc-controlls-container">
+                <button className="calc-item"></button>
+                <button className="calc-item"></button>
+                <button className="calc-item active" id="action" data-value="+" onClick={(e) => handleClick(e.target)}>+</button>
+                <button className="calc-item active" id="action" data-value="x" onClick={(e) => handleClick(e.target)}>x</button>
+                <button className="calc-item active" id="action" data-value="-" onClick={(e) => handleClick(e.target)}>-</button>
+                <button className="calc-item active" id="action" data-value="/" onClick={(e) => handleClick(e.target)}>/</button>
+              </div>
+            </div>
+          </div>
+        </div>
       </section>
     </div>
   );
